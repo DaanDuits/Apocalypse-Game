1b9d29e93324eff47cbd0596d8e420660c576189
DaanDuits <90894914+DaanDuits@users.noreply.github.com>
2024-01-24 18:57:21 +0100

    Added object pooling

---
+++ Assets/Scripts/Tests/ObjectPooling/NoObjectPooling.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Tests.ObjectPooling
+{
+    public class NoObjectPooling : MonoBehaviour
+    {
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private int maxAmount = 50;
+        private void Start()
+        {
+            for (int i = 0; i < maxAmount; i++)
+            {
+                Instantiate(prefab, this.transform).SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tools/ObjectPooling/ObjectPool.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+
+namespace Tools.ObjectPooling
+{
+    public class ObjectPool : MonoBehaviour
+    {
+        [SerializeField] private GameObject poolPrefab;
+        [SerializeField] private int maxAmount = 50;
+        [SerializeField] private int amountPerFrame = 5;
+
+        private void Start()
+        {
+            StartCoroutine(InstantiateNewPrefabs(maxAmount / amountPerFrame));
+        }
+
+        public void RequestObject() => RequestObject(transform.position, quaternion.identity);
+        public void RequestObject(Vector3 position, Quaternion rotation)
+        {
+            foreach (Transform child in transform)
+            {
+                if (!child.gameObject.activeSelf)
+                {
+                    child.gameObject.SetActive(true);
+                    child.position = position;
+                    child.rotation = rotation;
+                    return;
+                }
+            }
+
+            StartCoroutine(InstantiateNewPrefabs());
+            RequestObject(position, rotation);
+        }
+
+        public void DestroyObject(GameObject objectToDestroy)
+        {
+            objectToDestroy.SetActive(false);
+        }
+
+        private IEnumerator InstantiateNewPrefabs() => InstantiateNewPrefabs(1);
+        private IEnumerator InstantiateNewPrefabs(int loopAmount)
+        {
+            for (int i = 0; i < loopAmount; i++)
+            {
+                int amount = 0;
+                while (amount < amountPerFrame)
+                {
+                    Instantiate(poolPrefab, this.transform).SetActive(false);
+                    amount++;
+                }
+
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
