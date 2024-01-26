583b8b837a31aa9b50b4e8e7fed5f2f5eacb5cd2
DaanDuits <90894914+DaanDuits@users.noreply.github.com>
2024-01-26 22:12:57 +0100

    Revert "Removed Test directories"
    This reverts commit fd37f3ce8f4fa80e553b583a830dd991a0094963.

---
+++ Assets/Scripts/Tests/StateMachine/ActiveState.cs
@@ -0,0 +1,18 @@
+﻿using Tools.StateMachine;
+using UnityEngine;
+
+namespace Tests.StateMachine
+{
+    public class ActiveState : TestState
+    {
+        public override void StateStart()
+        {
+            spriteRenderer.color = Color.green;
+        }
+
+        public override void StateUpdate()
+        {
+            
+        }
+    }
+}
