ad310de6ed9377682f79dbff43f007669ae37822
DaanDuits <90894914+DaanDuits@users.noreply.github.com>
2024-01-26 22:11:40 +0100

    Revert "Removed Test directories"
    This reverts commit fd37f3ce8f4fa80e553b583a830dd991a0094963.

---
+++ Assets/Scripts/Tests/StateMachine/TestState.cs
@@ -0,0 +1,10 @@
+﻿using Tools.StateMachine;
+using UnityEngine;
+
+namespace Tests.StateMachine
+{
+    public abstract class TestState : State
+    {
+        [SerializeField] protected SpriteRenderer spriteRenderer;
+    }
+}
---
+++ Assets/Scripts/Tests/StateMachine/TestStateMachineManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Tools.StateMachine;
+using UnityEngine;
+
+namespace Tests.StateMachine
+{
+    public class TestStateMachineManager : StateMachineManager<TestState>
+    {
+        private void OnMouseEnter()
+        {
+            SwitchState(typeof(MonoBehaviour));
+        }
+
+        private void OnMouseExit()
+        {
+            SwitchState(typeof(InactiveState));
+        }
+
+        public void Test()
+        {
+            Debug.Log("Invoked Event");
+        }
+    }
+}
