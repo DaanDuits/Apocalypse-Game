9654507447a99cd1281af8b025adb72c0817fa37
DaanDuits <90894914+DaanDuits@users.noreply.github.com>
2024-02-06 16:57:07 +0100

    Camera movement optimalisation

--- Assets/Scripts/CameraMovement/CameraMovement.cs
+++ Assets/Scripts/CameraMovement/CameraMovement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
 using Input = UnityEngine.Input;
 
@@ -6,35 +7,39 @@ namespace CameraMovement
 {
     public class CameraMovement : MonoBehaviour
     {
-        [SerializeField] [Range(0.2f, 2f)] private float moveSpeed = 1;
+        private bool _isDragging;
         
         private Vector3 _dragOrigin;
-        private bool _isDragging;
         private Vector3 _difference;
+
+        private Vector3 MouseWorldPos => Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        private Vector3 _mouseOrigin;
+        private Vector3 _mouseDifference;
 
         private void Update()
         {
             if (Input.GetMouseButton(2))
             {
-                _difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-
+                _difference =  MouseWorldPos - transform.position;
+                
                 if (!_isDragging)
                 {
                     _isDragging = true;
-                    _dragOrigin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    _dragOrigin = MouseWorldPos;
+                    _mouseOrigin = Input.mousePosition;
                 }
+                
+                _mouseDifference = Input.mousePosition - _mouseOrigin;
             }
-            else
+            else if (Input.GetMouseButtonUp(2))
             {
                 _isDragging = false;
             }
 
-            if (_isDragging)
+            if (_isDragging && _mouseDifference != Vector3.zero)
             {
-                transform.position = _dragOrigin - _difference * moveSpeed;
-                
+                transform.position = _dragOrigin - _difference;
             }
-            
         }
     }
 }
